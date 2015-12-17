630b89006c677675a8ca37889278619452bdd7cf
domanhtien2011 <domanhtien2011@gmail.com>
2015-12-17 14:19:57 +0700

    Completely add pin functionality

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,50 @@
+class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+       flash[:notice] = "Pin was successfully updated!"
+      redirect_to @pin
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      flash[:notice] = "Successfully created new Pin"
+      redirect_to @pin
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+end
